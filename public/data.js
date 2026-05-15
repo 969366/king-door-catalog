d40509f18dbbcb7d2a7ddd72aed87a2fc29208c7
Mr.Micheal <khdoor@126.com>
2026-05-15 16:55:31 +0800

    KingDoor Auto Guard Update: 周五 2026/05/15 16:55:30.96

---
+++ public/data.js
@@ -0,0 +1,165 @@
+const productData = {
+    "aluminum-door": [],
+    "driveway-gate": [
+        {
+            "id": "DG-001",
+            "pure": "images/driveway-gate-DG-001-pure.webp",
+            "effect": "images/driveway-gate-DG-001-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-002",
+            "pure": "images/driveway-gate-DG-002-pure.webp",
+            "effect": "images/driveway-gate-DG-002-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-003",
+            "pure": "images/driveway-gate-DG-003-pure.webp",
+            "effect": "images/driveway-gate-DG-003-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-004",
+            "pure": "images/driveway-gate-DG-004-pure.webp",
+            "effect": "images/driveway-gate-DG-004-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-005",
+            "pure": "images/driveway-gate-DG-005-pure.webp",
+            "effect": "images/driveway-gate-DG-005-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-006",
+            "pure": "images/driveway-gate-DG-006-pure.webp",
+            "effect": "images/driveway-gate-DG-006-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-007",
+            "pure": "images/driveway-gate-DG-007-pure.webp",
+            "effect": "images/driveway-gate-DG-007-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-008",
+            "pure": "images/driveway-gate-DG-008-pure.webp",
+            "effect": "images/driveway-gate-DG-008-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-009",
+            "pure": "images/driveway-gate-DG-009-pure.webp",
+            "effect": "images/driveway-gate-DG-009-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-010",
+            "pure": "images/driveway-gate-DG-010-pure.webp",
+            "effect": "images/driveway-gate-DG-010-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-011",
+            "pure": "images/driveway-gate-DG-011-pure.webp",
+            "effect": "images/driveway-gate-DG-011-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "DG-012",
+            "pure": "images/driveway-gate-DG-012-pure.webp",
+            "effect": "images/driveway-gate-DG-012-pure.webp",
+            "isNew": false
+        }
+    ],
+    "pergola": [
+        {
+            "id": "PERG-002",
+            "pure": "images/pergola-PERG-002-pure.webp",
+            "effect": "images/pergola-PERG-002-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "PERG-003",
+            "pure": "images/pergola-PERG-003-pure.webp",
+            "effect": "images/pergola-PERG-003-pure.webp",
+            "isNew": false
+        }
+    ],
+    "pivot": [
+        {
+            "id": "P-001",
+            "pure": "images/pivot-P-001-pure.webp",
+            "effect": "images/pivot-P-001-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-002",
+            "pure": "images/pivot-P-002-pure.webp",
+            "effect": "images/pivot-P-002-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-003",
+            "pure": "images/pivot-P-003-pure.webp",
+            "effect": "images/pivot-P-003-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-004",
+            "pure": "images/pivot-P-004-pure.webp",
+            "effect": "images/pivot-P-004-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-005",
+            "pure": "images/pivot-P-005-pure.webp",
+            "effect": "images/pivot-P-005-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-006",
+            "pure": "images/pivot-P-006-pure.webp",
+            "effect": "images/pivot-P-006-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-007",
+            "pure": "images/pivot-P-007-pure.webp",
+            "effect": "images/pivot-P-007-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-008",
+            "pure": "images/pivot-P-008-pure.webp",
+            "effect": "images/pivot-P-008-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-009",
+            "pure": "images/pivot-P-009-pure.webp",
+            "effect": "images/pivot-P-009-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-010",
+            "pure": "images/pivot-P-010-pure.webp",
+            "effect": "images/pivot-P-010-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-011",
+            "pure": "images/pivot-P-011-pure.webp",
+            "effect": "images/pivot-P-011-pure.webp",
+            "isNew": false
+        },
+        {
+            "id": "P-012",
+            "pure": "images/pivot-P-012-pure.webp",
+            "effect": "images/pivot-P-012-pure.webp",
+            "isNew": false
+        }
+    ]
+};
